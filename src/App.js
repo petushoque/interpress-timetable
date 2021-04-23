dd68a2e485810e2903ecbe99af0930e0499e3d28
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-04-23 15:58:17 +0700

    feat: добавить селектор вида часов, их счетчик и инпуты с датой начала и концца обучения, по умолчанию в дате начала - сегодняшняя дата, кнопка отправить формирует объект с введенными данными для отправки

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState({
+    'hoursType': '',
+    'totalHours': '',
+    'startDate': ''
+  });
+  function handleAllData () {
+    console.log(data)
+    setData({
+      'hoursType': hoursType,
+      'totalHours': totalHours,
+      'startDate': startDate
+    })
+    console.log(data)
+  }
+
+  const [hoursType, setHoursType] = useState('academic');
+  function handleChangeHoursType (e) {
+    setHoursType(e.target.value)
+  }
+
+  const [totalHours, setTotalHours] = useState(0)
+  function handlePlusTotalHours () {
+    setTotalHours(totalHours+1)
+  }
+  function handleMinusTotalHours () {
+    if (totalHours>0) {
+      setTotalHours(totalHours-1)
+    }
+    else {
+      return totalHours
+    }
+  }
+
+  let Data = new Date()
+  const today = Data.toISOString().slice(0,10);
+  const [startDate, setStartDate] = useState(today)
+
+  function handleChangeStartDate (e) {
+    setStartDate(e.target.value)
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form>
+      <select name="hourstype" size="1" defaultValue='academic' onChange={handleChangeHoursType}>
+      <option value="academic">Академические</option>
+      <option value="astronomical">Астрономические</option>
+      </select>
+      </form>
+      <h1>{hoursType==='academic'? 'ACADEM' : 'ASTRO'}</h1>
+
+      <div className='total-hours'>
+        <button onClick={handleMinusTotalHours}>-</button>
+        <p>{totalHours}</p>
+        <p>Всего часов</p>
+        <button onClick={handlePlusTotalHours}>+</button>
+      </div>
+
+      <div className='dates'>
+        <input value={startDate} onChange={handleChangeStartDate} type='date'/>
+        <p>до</p>
+        <input type='date'/>
+      </div>
+
+
+
+
+<button onClick={handleAllData}>ОТПРАВИТЬ</button>
+      </div>
+    
   );
 }
 
