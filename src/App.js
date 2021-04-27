5a20f8c77581e2994335a678f3d96b1db8dd0233
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-04-27 11:24:34 +0700

    feat: начать верстку кнопок дней недели, их стейты записываются в массив

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import SelectList from './components/SelectList'
 import Counter from './components/Counter'
 import Timer from './components/Timer'
+import Days from './components/Days'
 
 function App() {
 
@@ -55,7 +56,7 @@ function App() {
 
 function endDateFunc () {
   if (data['total-hours']!==0 && data['hours-per-day']!==0 && 
-    (visits.mo && visits.tu && visits.we && visits.th && visits.fr && visits.sa && visits.su)!==0 )     
+    (visits[1] && visits[2] && visits[3] && visits[4] && visits[5] && visits[6] && visits[7])!==0 )     
     
     {
 
@@ -63,19 +64,19 @@ function endDateFunc () {
   let start = new Date(data['start-date']);
   let daysCount = Math.ceil(data['total-hours']/data['hours-per-day']);
   let days = [
-    'su',
-    'mo',
-    'tu',
-    'we',
-    'th',
-    'fr',
-    'sa'
+    7,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6
   ];
   let dateForCounter = start;
   let counter = daysCount;
 
   //чиcтит баг, с выбранными датой, количеством часов и пустым полем дней недели
-  if (!(visits.mo || visits.tu || visits.we || visits.th || visits.fr || visits.sa || visits.su))
+  if (!(visits[1] || visits[2] || visits[3] || visits[4] || visits[5] || visits[6] || visits[7]))
   {
     counter=0;
   }
@@ -96,45 +97,37 @@ function endDateFunc () {
   }
 }
 
-  const [visits, setVisits] = useState({
-    mo: false,
-    tu: false,
-    we: false,
-    th: false,
-    fr: false,
-    sa: false,
-    su: false,
-  })
+  const [visits, setVisits] = useState([])
+
+  function handleChanggeDayOfVisits (day) {
+    //if (day==='mowefr') {
+    //  if (!(visits.includes(1))) {
+    //    console.log('+')
+    //    setVisits([...visits, 1])
+    //  }
+    //  if (!(visits.includes(3))) {
+    //    setVisits([...visits, 3])
+    //  }
+    //  if (!(visits.includes(5))) {
+    //    setVisits([...visits, 5])
+    //  }
+    //  setVisits(visits.filter((n) => {return (n===1 || n===3 || n===5)}))   
+    //}
+    //if (day==='tuth') {
+    //  if (!(visits.includes(2))) {
+    //    setVisits([...visits, 2])
+    //  }
+    //  if (!(visits.includes(4))) {
+    //    setVisits([...visits, 4])
+    //  }
+    //  setVisits(visits.filter((n) => {return (n==2 || n==4)}))   
+    //}
+    
+    if (day > 0) {
+    visits.includes(day) ? setVisits(visits.filter(item => item !== day)) : setVisits([...visits, day]);
+    }
+  }
   
-  function handleChangeMnWeFr () {
-    setVisits({...visits, mo: true, tu: false, we: true, th: false, fr: true, sa: false, su: false});
-  }
-  function handleChangeTuTh () {
-    setVisits({...visits, mo: false, tu: true, we: false, th: true, fr: false, sa: false, su: false});
-  }
-
-  function handleChangeMonday () {
-    visits.mo === true ? setVisits({...visits, mo: false}) : setVisits({...visits, mo: true});
-  }
-  function handleChangeTuesday () {
-    visits.tu === true ? setVisits({...visits, tu: false}) : setVisits({...visits, tu: true});
-  }
-  function handleChangeWednesday () {
-    visits.we === true ? setVisits({...visits, we: false}) : setVisits({...visits, we: true});
-  }
-  function handleChangeThursday () {
-    visits.th === true ? setVisits({...visits, th: false}) : setVisits({...visits, th: true});
-  }
-  function handleChangeFriday () {
-    visits.fr === true ? setVisits({...visits, fr: false}) : setVisits({...visits, fr: true});
-  }
-  function handleChangeSaturday () {
-    visits.sa === true ? setVisits({...visits, sa: false}) : setVisits({...visits, sa: true});
-  }
-  function handleChangeSunday () {
-    visits.su === true ? setVisits({...visits, su: false}) : setVisits({...visits, su: true});
-  }
-
   function handleChangeRecreationTime (e) {
     setData({...data, 'recreation-time': e.target.value})
   }
@@ -142,13 +135,13 @@ function endDateFunc () {
 
   function handlePlusHoursPerDay () {
     if (data['hours-per-day'] < data['total-hours']) {
-      const replacement = data['hours-per-day'] + 1
+      const replacement = (data['hours-per-day'] + 1)
       setData({...data, 'hours-per-day': replacement})
       
     }
     else {
-      return
-      //return data['hours-per-day']
+      //return
+      return data['hours-per-day']
     }
   }
 
@@ -158,8 +151,7 @@ function endDateFunc () {
       setData({...data, 'hours-per-day': replacement})      
     }
     else {
-      return
-      //eturn data['hours-per-day']
+      return data['hours-per-day']
     }
   }
   
@@ -168,24 +160,9 @@ function endDateFunc () {
   }
 
   useEffect(() => {
-    if (data['recreation-time'])
+    //if (data['hours-per-day'])
       handleChangeEndTime ()
-  }, [data['recreation-time']])
-
-  useEffect(() => {
-    if (data['start-date'])
-      handleChangeEndTime ()
-  }, [data['start-date']])
-
-  useEffect(() => {
-    if (data['hours-type'])
-      handleChangeEndTime ()
-  }, [data['hours-type']])
-
-  useEffect(() => {
-    if (data['hours-per-day'])
-      handleChangeEndTime() 
-  }, [data['hours-per-day']])
+  }, [data['recreation-time'], data['start-time'], data['hours-type'], data['hours-per-day']])
 
   
 
@@ -259,17 +236,18 @@ function endDateFunc () {
 
         <Timer start={data['start-date']} finish={data['end-date']} handler={handleChangeStartDate} type='date'/>
 
+        <Days handler={handleChanggeDayOfVisits} visits={visits}></Days>
       
       <div className='schedule-of-visits'>
-          <button className='day-of-visits' onClick={handleChangeMnWeFr}>ПН/СР/ПТ</button>
-          <button className='day-of-visits' onClick={handleChangeTuTh}>ВТ/ЧТ</button>
-          <button className={visits.mo? 'day-of-visits_active' : 'day-of-visits'} onClick={handleChangeMonday}>ПН</button>
-          <button className={visits.tu? 'day-of-visits_active' : 'day-of-visits'} onClick={handleChangeTuesday}>ВТ</button>
-          <button className={visits.we? 'day-of-visits_active' : 'day-of-visits'} onClick={handleChangeWednesday}>СР</button>
-          <button className={visits.th? 'day-of-visits_active' : 'day-of-visits'} onClick={handleChangeThursday}>ЧТ</button>
-          <button className={visits.fr? 'day-of-visits_active' : 'day-of-visits'} onClick={handleChangeFriday}>ПТ</button>
-          <button className={visits.sa? 'day-of-visits_active' : 'day-of-visits'} onClick={handleChangeSaturday}>СБ</button>
-          <button className={visits.su? 'day-of-visits_active' : 'day-of-visits'} onClick={handleChangeSunday}>ВС</button>
+          <button className='day-of-visits' onClick={() => handleChanggeDayOfVisits('mowefr')}>ПН/СР/ПТ</button>
+          <button className='day-of-visits' onClick={() => handleChanggeDayOfVisits('tuth')}>ВТ/ЧТ</button>
+          <button className={visits.includes(1)? 'day-of-visits_active' : 'day-of-visits'} onClick={() => handleChanggeDayOfVisits(1)}>ПН</button>
+          <button className={visits.includes(2)? 'day-of-visits_active' : 'day-of-visits'} onClick={() => handleChanggeDayOfVisits(2)}>ВТ</button>
+          <button className={visits.we? 'day-of-visits_active' : 'day-of-visits'} onClick={() => handleChanggeDayOfVisits(3)}>СР</button>
+          <button className={visits.th? 'day-of-visits_active' : 'day-of-visits'} onClick={() => handleChanggeDayOfVisits(4)}>ЧТ</button>
+          <button className={visits.fr? 'day-of-visits_active' : 'day-of-visits'} onClick={() => handleChanggeDayOfVisits(5)}>ПТ</button>
+          <button className={visits.sa? 'day-of-visits_active' : 'day-of-visits'} onClick={() => handleChanggeDayOfVisits(6)}>СБ</button>
+          <button className={visits.su? 'day-of-visits_active' : 'day-of-visits'} onClick={() => handleChanggeDayOfVisits(7)}>ВС</button>
       </div>
 
       <Counter handlerMinus={handleMinusHoursPerDay} handlerPlus={handlePlusHoursPerDay} digit={data['hours-per-day']} description='Часов в день'/>
---
+++ src/components/Days.js
@@ -0,0 +1,20 @@
+import './Days.css'
+
+function Days (props) {
+    function handlerFunc (arg) {props.handler(arg)}
+    return (
+        <div className='days'>
+            <div className='days__day_active' onClick={() => handlerFunc('mowefr')}>ПН/СР/ПТ</div>
+            <div className='days__day_active' onClick={() => handlerFunc('tuth')}>ВТ/ЧТ</div>
+            <div className={props.visits.includes(1)? 'days__day_active' : 'days__day'} onClick={() => handlerFunc(1)}>ПН</div>
+            <div className={props.visits.includes(2)? 'days__day_active' : 'days__day'} onClick={() => handlerFunc(2)}>ВТ</div>
+            <div className={props.visits.includes(3)? 'days__day_active' : 'days__day'} onClick={() => handlerFunc(3)}>СР</div>
+            <div className={props.visits.includes(4)? 'days__day_active' : 'days__day'} onClick={() => handlerFunc(4)}>ЧТ</div>
+            <div className={props.visits.includes(5)? 'days__day_active' : 'days__day'} onClick={() => handlerFunc(5)}>ПТ</div>
+            <div className={props.visits.includes(6)? 'days__day_active' : 'days__day'} onClick={() => handlerFunc(6)}>СБ</div>
+            <div className={props.visits.includes(7)? 'days__day_active' : 'days__day'} onClick={() => handlerFunc(7)}>ВС</div>
+        </div>
+    )
+}
+
+export default Days
